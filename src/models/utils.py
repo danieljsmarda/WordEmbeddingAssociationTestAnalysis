980cbe16b19356fa12348990bf597d9ebeb82044
Daniel Smarda <danieljsmarda@gmail.com>
2020-11-22 12:30:32 +0100

    Add quantiles dict for biases

--- src/models/utils.py
+++ src/models/utils.py
@@ -14,7 +14,7 @@ def open_pickle(FILEPATH):
     return obj
 
 def save_arrays(FILEPATH, exp_num, order, X_metrics, Y_metrics, threshold,
-    pct_5, pct_95, A_biases, lower_bound, upper_bound):
+    pct_5, pct_95, A_biases, lower_bound, upper_bound, QR_dict):
     results_dict = open_pickle(FILEPATH)
     results_dict[exp_num] = results_dict.get(exp_num, defaultdict(dict))
     order_dict = results_dict[exp_num].get(order, {})
@@ -23,6 +23,7 @@ def save_arrays(FILEPATH, exp_num, order, X_metrics, Y_metrics, threshold,
     order_dict['X_mean'] = np.mean(X_metrics)
     order_dict['Y_mean'] = np.mean(Y_metrics)
     order_dict['threshold'] = threshold
+    order_dict['CI_dict'] = QR_dict
     #order_dict['pct_5'] = pct_5
     #order_dict['pct_95'] = pct_95
     #order_dict['A_biases'] = A_biases
