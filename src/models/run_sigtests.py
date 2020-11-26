e1d2e35f02578ee6b06432eba00d28fe8a99ff10
Daniel Smarda <danieljsmarda@gmail.com>
2020-11-26 18:48:33 +0100

    run_STs: Add assertion failure messages

--- src/models/run_sigtests.py
+++ src/models/run_sigtests.py
@@ -64,8 +64,8 @@ def get_n_test_stats(wv_obj, X_terms, Y_terms, A_terms, B_terms, n_samples=100):
     sigtest_dist_3 = []
     n_targets = len(X_terms)
     n_attributes = len(A_terms)
-    assert len(X_terms) == len(Y_terms)
-    assert len(A_terms) == len(B_terms)
+    assert len(X_terms) == len(Y_terms), "Target list lengths are unequal."
+    assert len(A_terms) == len(B_terms), "Attribute list lengths are unequal."
     vocab_list = list(wv_obj.vocab)
     random.seed(5)
     for i in tqdm(range(n_samples), desc=f'Calculating SigTest values for {n_samples} samples'):
